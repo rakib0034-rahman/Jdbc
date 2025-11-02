9878ff5e1c4d3265de3056a242fd389558fecb72
Rakib Rahman <rahmanrakib24@gmail.com>
2025-11-01 21:18:51 -0400

    properties file added

--- jdbcConnection/src/test/java/ConnectionJdbc.java
+++ jdbcConnection/src/test/java/ConnectionJdbc.java
@@ -1,19 +1,35 @@
 import org.testng.annotations.Test;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class ConnectionJdbc {
-    // Database configuration
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "Tnet@101530";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/classicmodels";
-    private static final String QUERY = "SELECT firstname, lastname FROM employees WHERE employeeNumber = 1621";
+    private static Properties loadProperties() throws IOException {
+        Properties prop = new Properties();
+        try (InputStream input = ConnectionJdbc.class.getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new IOException("Unable to find config.properties");
+            }
+            prop.load(input);
+        }
+        return prop;
+    }
 
     @Test
-    public void mysqlConnectionTest() throws SQLException {
-        // Use try-with-resources to ensure proper closure of JDBC resources
-        try (Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+    public void mysqlConnectionTest() throws SQLException, IOException {
+        // Load database properties
+        Properties prop = loadProperties();
+        String query = "SELECT firstname, lastname FROM employees WHERE employeeNumber = 1621";
+
+        // Use try-with-resources for database connection
+        try (Connection connection = DriverManager.getConnection(
+                prop.getProperty("db.url"),
+                prop.getProperty("db.username"),
+                prop.getProperty("db.password"));
              Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(QUERY)) {
+             ResultSet resultSet = statement.executeQuery(query)) {
 
             if (resultSet.next()) {
                 String firstName = resultSet.getString("firstname");
@@ -23,6 +39,5 @@ public class ConnectionJdbc {
                 System.out.println("Last Name: " + lastName);
             }
         }
-        // SQLException is thrown from the method signature
     }
 }
